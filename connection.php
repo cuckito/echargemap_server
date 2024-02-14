43d50320b0355f25f6239a1ffb6153db02d67294
Cuckito <64689435+cuckito@users.noreply.github.com>
2024-02-14 13:40:43 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $host = "localhost"; //IP BASE DE DATOS
 $root = "admin_web"; //USUARIO BASE DE DATOS
-$password = "@AP3btaN]ecCEGj*"; //CONTRASEÑA BASE DE DATOS
+$password = "PASSWORD*"; //CONTRASEÑA BASE DE DATOS
 $db = "echargemap"; //NOMBRE DE LA BASE DE DATOS
 
 $con = new mysqli($host, $root, $password, $db);
